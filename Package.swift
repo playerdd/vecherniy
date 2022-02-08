16e9ff9c749e1a593dc1a11a8bd686751de260e5
playerdd <voronindd@gmail.com>
2022-02-08 23:03:40 +0300

    sss

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
                 // Enable better optimizations when building in Release configuration. Despite the use of
                 // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
                 // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+               
             ]
         ),
         .executableTarget(name: "Run", dependencies: [.target(name: "App")]),
